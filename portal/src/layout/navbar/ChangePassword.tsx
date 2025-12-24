a1949941ccb1c749a69592f6c9e50b90cecd1c85
kathan <kathans@cybit.in>
2025-12-24 18:19:34 +0530

    ui changes

--- portal/src/layout/navbar/ChangePassword.tsx
+++ portal/src/layout/navbar/ChangePassword.tsx
@@ -47,88 +47,93 @@ export default function ChangePassword({ setIsOpen }) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-wrap">
-      {/* Old Password */}
-      <div className="float-left mb-[15px] px-[12px] w-full">
-        <label className="block mb-[8px] font-[500] text-[#1f2937] text-[14px]">
-          Current Password
-          <span className="ml-1 text-red-500">*</span>
-        </label>
-        <input
-          type="password"
-          placeholder="Enter old password"
-          {...register("old_password", {
-            required: "Old password is required",
-          })}
-          className="px-3 py-2 border border-[#ced4da] focus:border-[#86b7fe] rounded-md outline-none w-full"
-        />
-        {errors.old_password && (
-          <p className="mt-[4px] text-[12px] text-red-500">
-            {errors.old_password.message}
-          </p>
-        )}
-      </div>
+    <div className="flex justify-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-wrap max-w-[500px]"
+      >
+        {/* Old Password */}
+        <div className="float-left mb-[15px] px-[12px] w-full">
+          <label className="block mb-[8px] font-[500] text-[#1f2937] text-[14px]">
+            Current Password
+            <span className="ml-1 text-red-500">*</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter old password"
+            {...register("old_password", {
+              required: "Old password is required",
+            })}
+            className="px-3 py-2 border border-[#ced4da] focus:border-[#86b7fe] rounded-md outline-none w-full"
+          />
+          {errors.old_password && (
+            <p className="mt-[4px] text-[12px] text-red-500">
+              {errors.old_password.message}
+            </p>
+          )}
+        </div>
 
-      {/* New Password */}
-      <div className="float-left mb-[15px] px-[12px] w-full">
-        <label className="block mb-[8px] font-[500] text-[#1f2937] text-[14px]">
-          New Password
-          <span className="ml-1 text-red-500">*</span>
-        </label>
-        <input
-          type="password"
-          placeholder="Enter new password"
-          {...register("new_password", {
-            required: "New password is required",
-            pattern: {
-              value: passwordRegex,
-              message:
-                "Min 8 chars, 1 uppercase, 1 lowercase, 1 number & 1 special character required",
-            },
-          })}
-          className="px-3 py-2 border border-[#ced4da] focus:border-[#86b7fe] rounded-md outline-none w-full"
-        />
-        {errors.new_password && (
-          <p className="mt-[4px] text-[12px] text-red-500">
-            {errors.new_password.message}
-          </p>
-        )}
-      </div>
-      {/* Confirm Password */}
-      <div className="float-left mb-[15px] px-[12px] w-full">
-        <label className="block mb-[8px] font-[500] text-[#1f2937] text-[14px]">
-          Confirm Password
-          <span className="ml-1 text-red-500">*</span>
-        </label>
+        {/* New Password */}
+        <div className="float-left mb-[15px] px-[12px] w-full">
+          <label className="block mb-[8px] font-[500] text-[#1f2937] text-[14px]">
+            New Password
+            <span className="ml-1 text-red-500">*</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter new password"
+            {...register("new_password", {
+              required: "New password is required",
+              pattern: {
+                value: passwordRegex,
+                message:
+                  "Min 8 chars, 1 uppercase, 1 lowercase, 1 number & 1 special character required",
+              },
+            })}
+            className="px-3 py-2 border border-[#ced4da] focus:border-[#86b7fe] rounded-md outline-none w-full"
+          />
+          {errors.new_password && (
+            <p className="mt-[4px] text-[12px] text-red-500">
+              {errors.new_password.message}
+            </p>
+          )}
+        </div>
+        {/* Confirm Password */}
+        <div className="float-left mb-[15px] px-[12px] w-full">
+          <label className="block mb-[8px] font-[500] text-[#1f2937] text-[14px]">
+            Confirm Password
+            <span className="ml-1 text-red-500">*</span>
+          </label>
 
-        <input
-          type="password"
-          placeholder="Confirm new password"
-          {...register("confirm_password", {
-            required: "Confirm password is required",
-            validate: (value) =>
-              value === newPassword || "Passwords do not match",
-          })}
-          className="px-3 py-2 border border-[#ced4da] focus:border-[#86b7fe] rounded-md outline-none w-full"
-        />
+          <input
+            type="password"
+            placeholder="Confirm new password"
+            {...register("confirm_password", {
+              required: "Confirm password is required",
+              validate: (value) =>
+                value === newPassword || "Passwords do not match",
+            })}
+            className="px-3 py-2 border border-[#ced4da] focus:border-[#86b7fe] rounded-md outline-none w-full"
+          />
 
-        {errors.confirm_password && (
-          <p className="mt-[4px] text-[12px] text-red-500">
-            {errors.confirm_password.message}
-          </p>
-        )}
-      </div>
+          {errors.confirm_password && (
+            <p className="mt-[4px] text-[12px] text-red-500">
+              {errors.confirm_password.message}
+            </p>
+          )}
+        </div>
 
-      {/* Submit */}
-      <div className="mt-4 px-[12px] w-full">
-        <button
-          type="submit"
-          disabled={isSubmitting}
-          className="px-[20px] py-[12px] rounded-[8px] w-full primaryc-btn"
-        >
-          {isSubmitting ? "Updating..." : "Change Password"}
-        </button>
-      </div>
-    </form>
+        {/* Submit */}
+        <div className="mt-4 px-[12px] w-full">
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="px-[20px] py-[12px] rounded-[8px] w-full primaryc-btn"
+          >
+            {isSubmitting ? "Updating..." : "Change Password"}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
